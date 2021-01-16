93f4c2fd665e7b14ebfb87f83d11e8f1b75f579f
Nithin Danday <nithin.danday@icloud.com>
2021-01-15 22:56:29 -0800

    Seedbox WebTorrent

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const WebTorrent = require('webtorrent-hybrid')
+const EventEmitter = require('events');
+const process = require('process');
+const readline = require('readline');
+const { count } = require('console');
+const { cursorTo } = require('readline');
+const { stdout } = require('process');
+//require('events').EventEmitter.defaultMaxListeners = Infinity;
+var col = process.stdout.columns
 var client = new WebTorrent()
-var uri = 'magnet:?xt=urn:btih:6C50B8BBB257FF40B0986455DF56B78C87F19442&dn=Lil%20Uzi%20Vert%20-%20XO%20TOUR%20Llif3%20%5B2017-Single%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce'
+var uris = ['https://pl.gammaspectra.live/video_parler_segment_00.torrent', 'https://pl.gammaspectra.live/video_parler_segment_01.torrent']
+var speeds = [
+    ['total_downloaded'],
+    ['download_speed'],
+    ['progress'],
+    ['upload_speed']
+]
+console.clear()
+uris.forEach((uri, index) => {
+    client.add(uri, { path: './files/' }, (torrent) => {
 
-
-client.add(uri, { path: './files/' }, (torrent) => {
-    torrent.on('metadata', ()=> {
-        console.log('name: ' + torrent.name);
         
+        torrent.on('download', (bytes) => {
+            console.clear()
+            speeds[0][index+1] = Math.floor(torrent.downloaded/1000000)
+            speeds[1][index+1] = Math.floor((torrent.downloadSpeed)/1000)
+            speeds[2][index+1] = Math.floor(torrent.progress * 100) + '%'
+            console.table(speeds)
+            
+        })
+       torrent.on('upload', function (bytes) {
+            console.clear()
+            console.table(speeds)
+            speeds[3][index+1] = Math.floor(torrent.uploadSpeed/1000)
+        })
+    
     })
-    torrent.on('download', (bytes) => {
-        console.clear()
-        console.table([{total_downloaded: torrent.downloaded, download_speed: Math.floor((torrent.downloadSpeed)/1000), progress: Math.floor(torrent.progress * 100) + '%'}])
-    })
-    torrent.on('upload', (bytes) => {
-        
-    })
-})
-
-/* client.seed('./test.txt', (torrent) => {
-    console.log('Seeding...' + torrent.magnetURI)
-}) */
+});
